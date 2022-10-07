8101abd5668414841e011ddef2a23a1af05f24e9
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-10-06 19:27:45 -0600

    add glfw submodule

--- Engine/libs/glfwPremake5.lua
+++ Engine/libs/glfwPremake5.lua
@@ -6,30 +6,30 @@ project "GLFW"
 	targetdir "bin/%{cfg.buildcfg}"
 	objdir "bin-int/%{cfg.buildcfg}"
 
-	includedirs { "glfw/include/" }
+	includedirs { "windowCreation/include/" }
 
 	files
 	{
-		"glfw/include/GLFW/glfw3.h",
-		"glfw/include/GLFW/glfw3native.h",
-		"glfw/src/internal.h",
-		"glfw/src/platform.h",
-		"glfw/src/mappings.h",
-		"glfw/src/context.c",
-		"glfw/src/init.c",
-		"glfw/src/input.c",
-		"glfw/src/monitor.c",
-		"glfw/src/platform.c",
-		"glfw/src/vulkan.c",
-		"glfw/src/window.c",
-		"glfw/src/egl_context.c",
-		"glfw/src/osmesa_context.c",
-		"glfw/src/null_platform.h",
-		"glfw/src/null_joystick.h",
-		"glfw/src/null_init.c",
-		"glfw/src/null_monitor.c",
-		"glfw/src/null_window.c",
-		"glfw/src/null_joystick.c",
+		"windowCreation/include/GLFW/glfw3.h",
+		"windowCreation/include/GLFW/glfw3native.h",
+		"windowCreation/src/internal.h",
+		"windowCreation/src/platform.h",
+		"windowCreation/src/mappings.h",
+		"windowCreation/src/context.c",
+		"windowCreation/src/init.c",
+		"windowCreation/src/input.c",
+		"windowCreation/src/monitor.c",
+		"windowCreation/src/platform.c",
+		"windowCreation/src/vulkan.c",
+		"windowCreation/src/window.c",
+		"windowCreation/src/egl_context.c",
+		"windowCreation/src/osmesa_context.c",
+		"windowCreation/src/null_platform.h",
+		"windowCreation/src/null_joystick.h",
+		"windowCreation/src/null_init.c",
+		"windowCreation/src/null_monitor.c",
+		"windowCreation/src/null_window.c",
+		"windowCreation/src/null_joystick.c",
 	}
 
 	filter "system:windows"
@@ -38,18 +38,18 @@ project "GLFW"
 
 		files
 		{
-			"glfw/src/win32_init.c",
-			"glfw/src/win32_module.c",
-			"glfw/src/win32_joystick.c",
-			"glfw/src/win32_monitor.c",
-			"glfw/src/win32_time.h",
-			"glfw/src/win32_time.c",
-			"glfw/src/win32_thread.h",
-			"glfw/src/win32_thread.c",
-			"glfw/src/win32_window.c",
-			"glfw/src/wgl_context.c",
-			"glfw/src/egl_context.c",
-			"glfw/src/osmesa_context.c"
+			"windowCreation/src/win32_init.c",
+			"glwindowCreation/src/win32_module.c",
+			"windowCreation/src/win32_joystick.c",
+			"windowCreation/src/win32_monitor.c",
+			"windowCreation/src/win32_time.h",
+			"windowCreation/src/win32_time.c",
+			"windowCreation/src/win32_thread.h",
+			"windowCreation/src/win32_thread.c",
+			"windowCreation/src/win32_window.c",
+			"windowCreation/src/wgl_context.c",
+			"windowCreation/src/egl_context.c",
+			"windowCreation/src/osmesa_context.c"
 		}
 
 		defines 
