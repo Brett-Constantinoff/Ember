b5817e369ca740730a8c7b3b23f0fde9b7d503f5
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-11-02 17:14:06 -0600

    update engine.h

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -11,18 +11,21 @@
 #include "../../libs/imgui/imgui.h"
 #include "../../libs/imgui/imgui_impl_glfw.h"
 #include "../../libs/imgui/imgui_impl_opengl3.h"
+#include "../../libs/math/glm/glm.hpp"
 
 #include "src/Core/Application.h"
 #include "src/Core/Layer.h"
 #include "src/Core/LayerStack.h"
 #include "src/Core/Window.h"
-#include <glm/glm.hpp>
 
 #include "src/Renderer/VertexArray.h"
 #include "src/Renderer/Buffer.h"
 #include "src/Renderer/VertexBuffer.h"
 #include "src/Renderer/Shader.h"
 #include "src/Renderer/Camera.h"
+
+#include "src/Scene/SceneObject.h"
+#include "src/Scene/Cube.h"
 
 #include <iostream>
 #include <stdint.h>
