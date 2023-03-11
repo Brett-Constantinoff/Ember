92fa0bcb66da6810f3a18202fdf5325176365e0b
Brett Constantinoff <constantinoffbrett@gmail.com>
2023-03-11 11:14:13 -0700

    code refactor

--- Sandbox/Src/Test3D/Application3D.cpp
+++ Sandbox/Src/Test3D/Application3D.cpp
@@ -18,75 +18,103 @@ Application3D::~Application3D()
 
 void Application3D::run()
 {
-	onStart();																	// do any intial setup
+	// setup app
+	onStart();	
+
+	// main application loop
 	while (m_window->isOpen())
 	{
-		double currFrame = glfwGetTime();										// get current time
-		double dt = currFrame - m_lastFrame;									// calculate change in time (tick speed)
-		m_lastFrame = currFrame;												// update the last frame time
+		double currFrame = glfwGetTime();										
+		double dt = currFrame - m_lastFrame;									
+		m_lastFrame = currFrame;												
 
-		onUpdate(dt);															// update the application
-		onRender();																// render anything to the screen
+		onUpdate(dt);															
+		onRender();																
 
-		m_window->update();														// update our window
+		m_window->update();														
 	}
 }
 
 void Application3D::createWindow()
 {
-	Ember::Core::WindowCreateInfo windowCreateInfo;							
-	windowCreateInfo.m_api = "API_OPENGL";										// api version
-	windowCreateInfo.m_width = 1080;											// window width
-	windowCreateInfo.m_height = 1080;											// window height
-	windowCreateInfo.m_label = "Application 3D";								// give out app a label
-	windowCreateInfo.m_fov = 90.0f;												// set a FOV, for 3D perspective viewing
-	windowCreateInfo.m_near = 0.1f;												// set near plane
-	windowCreateInfo.m_far = 100.0f;											// set far plane
-	m_window = EMBER_NEW Ember::Core::Window(windowCreateInfo);					// create our new window
+	Ember::Core::WindowCreateInfo windowCreateInfo;		
+	// set our api
+	windowCreateInfo.m_api = "API_OPENGL";				
+	// give a width
+	windowCreateInfo.m_width = 1080;			
+	// give a height
+	windowCreateInfo.m_height = 1080;
+	// give our app a name
+	windowCreateInfo.m_label = "Application 3D";
+	// these next 3 are for perspective viwing
+	windowCreateInfo.m_fov = 90.0f;												
+	windowCreateInfo.m_near = 0.1f;												
+	windowCreateInfo.m_far = 100.0f;		
+	// create our new window
+	m_window = EMBER_NEW Ember::Core::Window(windowCreateInfo);					
 }
 
 void Application3D::createScene()
 {
 	Ember::Scene::CameraCreateInfo cameraCreateInfo;
-	cameraCreateInfo.m_pos = { 0.0f, 0.0f, 5.0f };								// camera starting position
-	cameraCreateInfo.m_front = { 0.0f, 0.0f, -1.0f };							// camera front vector (which way itll point)
-	cameraCreateInfo.m_up = { 0.0f, 1.0f, 0.0f };								// which way is up
-	cameraCreateInfo.m_yaw = -90.0f;											// side to side rotation
-	cameraCreateInfo.m_pitch = 0.0f;											// up down rotation
-	m_camera = EMBER_NEW Ember::Scene::Camera(cameraCreateInfo);				// create scene camera
+	// where is the camera located
+	cameraCreateInfo.m_pos = { 0.0f, 0.0f, 5.0f };		
+	// which was is forward
+	cameraCreateInfo.m_front = { 0.0f, 0.0f, -1.0f };	
+	// which way is up
+	cameraCreateInfo.m_up = { 0.0f, 1.0f, 0.0f };			
+	// rotate about y-axis
+	cameraCreateInfo.m_yaw = -90.0f;						
+	// rotate about x-axis
+	cameraCreateInfo.m_pitch = 0.0f;					
+	// create camera
+	m_camera = EMBER_NEW Ember::Scene::Camera(cameraCreateInfo);				
 
-	m_shader = EMBER_NEW Ember::Renderer::Shader(SHADER_PATH "flatShading.hlsl");// create scene shader
+	// create scene shader
+	m_shader = EMBER_NEW Ember::Renderer::Shader(SHADER_PATH "flatShading.hlsl");
 
-	Ember::Scene::SceneCreateInfo sceneCreateInfo;						
-	sceneCreateInfo.m_camera = m_camera;										// pass our camera to the scene
-	sceneCreateInfo.m_shader = m_shader;										// pass shader to the scene
-	sceneCreateInfo.m_windowContext = *m_window->getContext();					// pass our window context to the scene
-	m_scene = EMBER_NEW Ember::Scene::Scene(sceneCreateInfo);					// create our scene
+	Ember::Scene::SceneCreateInfo sceneCreateInfo;		
+	// give our scene a camera
+	sceneCreateInfo.m_camera = m_camera;			
+	// give our scene a shader
+	sceneCreateInfo.m_shader = m_shader;			
+	// pass a pointer to our window
+	sceneCreateInfo.m_windowContext = *m_window->getContext();			
+	// create scene
+	m_scene = EMBER_NEW Ember::Scene::Scene(sceneCreateInfo);					
 }
 
 void Application3D::createRenderer()
 {
 	Ember::Renderer::RendererCreateInfo rendererCreateInfo;
-	rendererCreateInfo.m_api = "API_OPENGL";									// api version (only one for now)
-	rendererCreateInfo.m_imguiEnabled = true;									// do we want GUI
-	rendererCreateInfo.m_scene = m_scene;										// pass our scene to the renderer
-	rendererCreateInfo.m_window = m_window;										// pass a pointer to our window
-	m_renderer = EMBER_NEW Ember::Renderer::Renderer(rendererCreateInfo);		// create our new renderer
+	// set a backend API
+	rendererCreateInfo.m_api = "API_OPENGL";			
+	// do we want a GUI
+	rendererCreateInfo.m_imguiEnabled = true;				
+	// give our renderer a scene to render
+	rendererCreateInfo.m_scene = m_scene;
+	// give our renderer a window to render to
+	rendererCreateInfo.m_window = m_window;		
+	// create our renderer
+	m_renderer = EMBER_NEW Ember::Renderer::Renderer(rendererCreateInfo);		
 }
 
 void Application3D::onStart()
 {
-	createWindow();																// create window
-	createScene();																// create scene
-	createRenderer();															// create renderer
+	// initial app creation
+	createWindow();																
+	createScene();																
+	createRenderer();															
 }
 
 void Application3D::onUpdate(double dt)
 {
+	// update things in our scene
 	m_renderer->update(dt);
 }
 
 void Application3D::onRender()
 {
+	// render our scene 
 	m_renderer->render();
 }
